1056e9948e53873dd1a833f36af01d19d3736820
daniel-5772 <daniel.acosta4578@gmail.com>
2021-11-09 22:47:37 -0500

    4to commit 09.11.2021

--- src/Components/Navbars.jsx
+++ src/Components/Navbars.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Container, Nav, Navbar} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
+
+
 const Navbars  = () => {
-    return(
+  const {loginWithRedirect, isAuthenticated, user, logout} = useAuth0();
+  const [textButton, setTextButton] = useState('Login')
+
+  const LoginBu = () => {
+ 
+    if(!isAuthenticated){
+      setTextButton('Login')
+    return loginWithRedirect()
+    }else{
+      setTextButton('Logout')
+      return logout({ returnTo: window.location.origin})
+    }
+  }
+
+
+  return(
         <div>
     <Navbar bg="primary" variant="dark">
     <Container>
-        <img className ="Logo" src="https://w7.pngwing.com/pngs/587/342/png-transparent-computer-icons-desktop-beauty-soap-text-logo-soap.png" alt="Logo" />
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        
+    <Navbar.Brand ><img className ="Logo" src="https://w7.pngwing.com/pngs/587/342/png-transparent-computer-icons-desktop-beauty-soap-text-logo-soap.png" alt="Logo" /></Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      
+      <Nav.Link><Link to ='/'>Home</Link></Nav.Link> 
+      <Nav.Link><Link to ='/features'>Features</Link></Nav.Link> 
+      
     </Nav>
     </Container>
+    <button 
+    onClick = {LoginBu()}
+    className="btn btn-primary">{textButton}</button>
   </Navbar>
         </div>
     )
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -10,6 +10,7 @@ import PublicLayout from '../Layout/PublicLayout';
 import Dashboard from '../Pages/Admin/Dashboard';
 import Roles from '../Pages/Admin/Roles';
 import Ventas from '../Pages/Admin/Ventas';
+import Features from '../Pages/Public/Features';
 import Index from '../Pages/Public/Index';
 
 const Routes  = () => {
@@ -32,9 +33,16 @@ const Routes  = () => {
                     </PrivateLayout>
 
                 </Route>
-                <Route path= '/'>
+                <Route path= {['/features','/']}>
                     <PublicLayout>
-                        <Index />
+                        <Switch>
+                            <Route path='/features'>
+                                <Features/>
+                            </Route>
+                            <Route path='/'>
+                                <Index/>
+                            </Route>                            
+                        </Switch>
                     </PublicLayout>
                 </Route>
 
